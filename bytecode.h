badaea90bc1b7b1880a6bec411bd6bd939a124e0
wittej <wittej@oregonstate.edu>
2023-10-19 16:12:27 -0700

    Add value
    Representation for bytecoe chunk and associated values.

--- bytecode.h
+++ bytecode.h
@@ -2,9 +2,15 @@
 #define LISP_BYTECODE_H
 
 #include "common.h"
+#include "value.h"
 
 enum class opcode {
 	RETURN,
 };
+
+typedef struct {
+	std::vector<opcode> code;
+	std::vector<Value> constants;
+} Chunk;
 
 #endif
