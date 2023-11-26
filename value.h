20981d3f513e25f5d4c7b717b2245bb128705db4
wittej <wittej@oregonstate.edu>
2023-11-26 14:08:38 -0800

    Beginning of s-expressions
    Basic thought for data - want cons/car/cdr and friends

--- value.h
+++ value.h
@@ -6,18 +6,29 @@
 enum class value_type {
 	BOOL,
 	NIL,
-	NUMBER
+	NUMBER,
+	LIST
 };
+
+struct Cons;
 
 struct Value {
 	value_type type;
 	union {
 		bool boolean;
 		double number;
+		Cons* list;
 	} as;
 	Value(bool val) : type{ value_type::BOOL }, as{ .boolean=val } {}
 	Value(double val) : type{ value_type::NUMBER }, as{ .number=val } {}
+	Value(Cons* val) : type{ value_type::LIST }, as{ .list = val } {}
 	Value() : type{ value_type::NIL }, as{ .boolean=false } {}
+};
+
+struct Cons {
+	Value car;
+	Value cdr;
+	Cons(Value car, Value cdr) : car{ car }, cdr{ cdr } {};
 };
 
 #endif
