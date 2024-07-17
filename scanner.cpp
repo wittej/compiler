e8139d5fcb33404c12be55024880938db9bacbe6
wittej <wittej@oregonstate.edu>
2024-07-17 16:27:01 -0700

    Cleaning and commenting scanner files
    Continuing my review of the code - aiming for readability and making notes for future development.

--- scanner.cpp
+++ scanner.cpp
@@ -1,5 +1,10 @@
 #include "scanner.h"
 
+/* Scan source and return the next token, or a token of type token_type::END if
+*  the entire source has been scanned.
+* 
+*  :return: next token in the source.
+*/
 Token
 Scanner::scan()
 {
@@ -22,21 +27,27 @@ Scanner::scan()
 		case '+':
 		case '-':
 			if (std::isdigit(source[current]) || source[current] == '.')
-				return scanNumber();
+				return scan_number();
 			else return makeToken(token_type::SYMBOL);
 		case '.':
-			if (std::isdigit(source[current])) return scanNumber();
+			if (std::isdigit(source[current])) return scan_number();
 			else return makeToken(token_type::SYMBOL);
 	}
 
-	if (std::isdigit(c)) return scanNumber();
-	if (std::isalpha(c)) return scanSymbol();
+	if (std::isdigit(c)) return scan_number();
+	if (std::isalpha(c)) return scan_alphanumeric();
 
-	return makeError("Unexpected character.");
+	return makeError("unexpected character '" + std::string(1, c) + "'");
 }
 
+/* Used by scan() if token form indicates it is a number. Currently only
+*  decimal numbers are supported, with an optional leading +/-. Increments
+*  the current index past the end of the token.
+* 
+*  :return: numeric token in decimal or integer format.
+*/
 Token
-Scanner::scanNumber()
+Scanner::scan_number()
 {
 	while (std::isdigit(source[current])) ++current;
 	if (source[current] == '.') ++current;
@@ -44,48 +55,60 @@ Scanner::scanNumber()
 	return makeToken(token_type::NUMBER);
 }
 
+/* Used by scan when an alphanumeric token is encountered - will classify
+*  and return the token. Increments the current index past the end of the
+*  token.
+* 
+*  :return: alphanumeric token.
+*/
 Token
-Scanner::scanSymbol()
+Scanner::scan_alphanumeric()
 {
 	while (std::isalnum(source[current])) ++current;
-	return makeToken(symbolType());
+	return makeToken(classify_alphanumeric());
 }
 
+/* Determines and returns the correct type for an alphanumeric token
+*  encountered during scan(). 
+* 
+*  :return: correct token_type for the current token being scanned.
+*/
 token_type
-Scanner::symbolType()
+Scanner::classify_alphanumeric()
 {
 	switch (source[start]) {
-	case 'a':
-		return checkKeyword(1, "nd", token_type::AND);
-	case 'd':
-		return checkKeyword(1, "efine", token_type::DEFINE);
-	case 'f':
-		return checkKeyword(1, "alse", token_type::FALSE);
-	case 'i':
-		return checkKeyword(1, "f", token_type::IF);
-	case 'l':
-		return checkKeyword(1, "ambda", token_type::LAMBDA);
-	case 'n':
-		if (current > start + 1) {
-			switch (source[start + 1]) {
-			case 'i':
-				return checkKeyword(2, "l", token_type::NIL);
-			case 'o':
-				return checkKeyword(2, "t", token_type::NOT);
+		case 'a':
+			return checkKeyword(1, "nd", token_type::AND);
+		case 'd':
+			return checkKeyword(1, "efine", token_type::DEFINE);
+		case 'f':
+			return checkKeyword(1, "alse", token_type::FALSE);
+		case 'i':
+			return checkKeyword(1, "f", token_type::IF);
+		case 'l':
+			return checkKeyword(1, "ambda", token_type::LAMBDA);
+		case 'n':
+			if (current > start + 1) {
+				switch (source[start + 1]) {
+					case 'i':
+						return checkKeyword(2, "l", token_type::NIL);
+					case 'o':
+						return checkKeyword(2, "t", token_type::NOT);
+				}
 			}
-		}
-		break;
-	case 'o':
-		return checkKeyword(1, "r", token_type::OR);
-	case 't':
-		return checkKeyword(1, "rue", token_type::TRUE);
+			break;
+		case 'o':
+			return checkKeyword(1, "r", token_type::OR);
+		case 't':
+			return checkKeyword(1, "rue", token_type::TRUE);
 	}
 
 	return token_type::SYMBOL;
 }
 
 token_type
-Scanner::checkKeyword(size_t index, std::string target, token_type type)
+Scanner::checkKeyword(const size_t index, const std::string target,
+					  const token_type type)
 {
 	if (current - start != index + target.length()) return token_type::SYMBOL;
 	if (source.substr(start + index, target.length()) == target) return type;
@@ -93,7 +116,7 @@ Scanner::checkKeyword(size_t index, std::string target, token_type type)
 }
 
 Token
-Scanner::makeToken(token_type type)
+Scanner::makeToken(const token_type type)
 {
 	std::string token_string = source.substr(start, current - start);
 
@@ -107,7 +130,7 @@ Scanner::makeToken(token_type type)
 }
 
 Token
-Scanner::makeError(std::string error)
+Scanner::makeError(const std::string error)
 {
 	return Token{ .type = token_type::ERROR, .string = error, .line = line };
 }
--- scanner.h
+++ scanner.h
@@ -3,6 +3,11 @@
 
 #include "common.h"
 
+// TODO: consider implementing logical operators as built-ins.
+
+/* Most tokens (including most built-in functions) are symbols and will be
+   resolved to values. Booleans, logical operators, numbers, and special forms
+   like ternary if and lambda declaration are their own type of token. */
 enum class token_type {
 	BEGIN, END, ERROR,
 	LPAREN, RPAREN,
@@ -12,12 +17,15 @@ enum class token_type {
 	DEFINE
 };
 
+/* Representation of a token - contains information needed for compilation and
+   error reporting. */
 struct Token {
 	token_type type;
 	std::string string;
 	size_t line;
 };
 
+/* Converts source file / input line to tokens. Provides one token at a time. */
 class Scanner {
 private:
 	std::string& source;
@@ -26,9 +34,9 @@ private:
 	size_t line = 1;
 	Token makeToken(token_type type);
 	Token makeError(std::string error);
-	Token scanNumber();
-	Token scanSymbol();
-	token_type symbolType();
+	Token scan_number();
+	Token scan_alphanumeric();
+	token_type classify_alphanumeric();
 	token_type checkKeyword(size_t index, std::string target, token_type type);
 public:
 	Scanner(std::string& source) :source{ source } {};
