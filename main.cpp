cb06a2f9e149921de797c7b2fd5bae541f202412
wittej <wittej@oregonstate.edu>
2024-08-16 22:33:00 -0700

    Tail call optimization idea
    Current idea - use static analysis to determine when a tail call is occuring.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,24 @@
 /*
 TODOs:
 
-Better reporting for compilation inside function
+Better reporting for compilation inside function - do during the new static
+	analysis step.
+
+Recursion - tail recursion basic idea: return and evaluate a tail call as
+	planned. However, this is pretty wasteful if it's not actually in a
+	"tail context". Whether a call is in a tail context can be determined
+	with static analysis in a post-processing step. (Read from bytecode -
+	see if it jumps to return. If so, it's a tail call.) Tail call is like
+	a regular call except we clean up the prior stack frame first.
+
 Add new features to test file as they are implemented
-Recursion
+	- set in all contexts.
+
 Error type
+
+Refactor memory out of VM.
+
+Refactor parser / compiler?
 */
 
 
